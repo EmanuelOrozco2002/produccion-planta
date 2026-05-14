fa8eff2f8d1d0d722f3e38bc31738f6e07f418a8
Emanuel Orozco <emanuelorozcotaborda@gmail.com>
2026-05-14 11:42:21 -0500

    update schemas empleado and registro_produccion and add validations

--- backend/app/schemas/empleado.py
+++ backend/app/schemas/empleado.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.schemas.registro_produccion import RegistroProduccionResponse
 from typing import List
 
 
 class EmpleadoCreate(BaseModel):
-    nombre: str
+    nombre: str = Field(min_length=3, max_length=50)
 
 
 class EmpleadoResponse(BaseModel):
--- backend/app/schemas/registro_produccion.py
+++ backend/app/schemas/registro_produccion.py
@@ -1,18 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 from datetime import datetime
 
 
 class RegistroProduccionCreate(BaseModel):
 
-    empleado_id: int
+    empleado_id: int = Field(gt=0)
 
-    proceso_id: int
+    proceso_id: int = Field(gt=0)
 
-    op_id: int
+    op_id: int = Field(gt=0)
 
     hora_inicio: datetime
 
     hora_fin: datetime
+
+    @model_validator(mode="after")
+    def validar_horas(self):
+        if self.hora_inicio >= self.hora_fin:
+            raise ValueError("La hora de inicio debe ser mayor a la hora final")
+        return self
 
 
 class RegistroProduccionResponse(BaseModel):
